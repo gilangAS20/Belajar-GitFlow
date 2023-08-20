f24746f431aeeb94d69cdd11577136ecbbde6d65
Gilang Agung Saputra <gilangagungs20@gmail.com>
2023-08-20 07:28:47 +0700

    edit first console print and add if logic

--- learn_cSharp_gitflow/Program.cs
+++ learn_cSharp_gitflow/Program.cs
@@ -7,10 +7,17 @@ namespace learn_csharp_gitflow{
     {
         public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Dunia Tipu-Tipu!");
             Console.WriteLine("Hello Gilang!");
             Console.WriteLine("Hello Agung!");
             Console.WriteLine("Hello Saputra!");
+
+            int startNumber = 0;
+
+            if(startNumber <= 0)
+            {
+                Console.WriteLine("print hasil dari if startNumber = 0");
+            }
         }
     }
 }
