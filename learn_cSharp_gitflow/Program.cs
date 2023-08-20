65c1b8fe14319c34cc320adedd141851db844382
Gilang Agung Saputra <gilangagungs20@gmail.com>
2023-08-20 16:01:23 +0700

    add new class and some text

--- learn_cSharp_gitflow/Program.cs
+++ learn_cSharp_gitflow/Program.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace learn_csharp_gitflow{
+namespace learn_csharp_gitflow
+{
     public class Program
     {
         public static void Main()
         {
-            Console.WriteLine("Hello yhu ahahaha slebew! (from develop) ");
+            Console.WriteLine("Hello yhu ahahaha slebew! (from develop), add some text from branch develop");
             Console.WriteLine("Hello Gilang Agung Saputra hahahahha!");
             Console.WriteLine("Hello Agung!");
             Console.WriteLine("Hello Saputra!");
@@ -19,6 +20,39 @@ namespace learn_csharp_gitflow{
                 Console.WriteLine("print hasil dari if startNumber = 0");
                 Console.WriteLine("Bombastic side eye (from op0003)");
             }
+
+            Calculator calculatorOne = new Calculator();
+            calculatorOne.Add(1, 2);
+            calculatorOne.Substract(1, 2);
+            calculatorOne.Multiply(1, 2);
+            calculatorOne.Divide(1, 2);
+        }
+    }
+
+    public class Calculator
+    {
+        public void Add(int numberOne, int numberTwo)
+        {
+            int total =  numberOne + numberTwo;
+            Console.WriteLine("total add: " + total);
+        }
+
+        public void Substract(int numberOne, int numberTwo)
+        {
+            int total =  numberOne - numberTwo;
+            Console.WriteLine("total substract: " + total);
+        }
+
+        public void Multiply(int numberOne, int numberTwo)
+        {
+            int total =  numberOne * numberTwo;
+            Console.WriteLine("total multiply: " + total);
+        }
+
+        public void Divide(int numberOne, int numberTwo)
+        {
+            int total =  numberOne / numberTwo;
+            Console.WriteLine("total divide: " + total);
         }
     }
 }
