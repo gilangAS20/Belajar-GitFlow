eb83dc64378d438bb8ff26e2490d0d89af298789
Gilang Agung Saputra <gilangagungs20@gmail.com>
2023-08-20 07:31:05 +0700

    edit first console print and startNumber value

--- learn_cSharp_gitflow/Program.cs
+++ learn_cSharp_gitflow/Program.cs
@@ -7,10 +7,17 @@ namespace learn_csharp_gitflow{
     {
         public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello yhu ahahaha slebew! (from develop) ");
             Console.WriteLine("Hello Gilang!");
             Console.WriteLine("Hello Agung!");
             Console.WriteLine("Hello Saputra!");
+
+            int startNumber = 1;
+
+            if(startNumber <= 0)
+            {
+                Console.WriteLine("print hasil dari if startNumber = 0, ditambahi beberapa kata");
+            }
         }
     }
 }
