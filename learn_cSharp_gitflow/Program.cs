6fdf528c18fb9a64c017ffc5b76a82fd69dcb7f8
Gilang Agung Saputra <gilangagungs20@gmail.com>
2023-08-20 07:06:55 +0700

    delete files inside bin folder

--- learn_cSharp_gitflow/Program.cs
+++ learn_cSharp_gitflow/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace learn_csharp_gitflow{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
