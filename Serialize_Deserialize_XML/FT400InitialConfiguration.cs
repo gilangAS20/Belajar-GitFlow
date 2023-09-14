76db9c13983f806a13577c4c0359accc65c63c23
Gilang Agung Saputra <gilangagungs20@gmail.com>
2023-09-15 05:58:32 +0700

    add serialization

---
+++ Serialize_Deserialize_XML/FT400InitialConfiguration.cs
@@ -0,0 +1,10 @@
+namespace Learn_Serialize_Deserialize_XML;
+
+public class FT400InitialConfiguration
+{
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public int Age { get; set; }
+    public DateTime JoiningDate { get; set; }
+    public bool IsAlive { get; set; }
+}
---
+++ Serialize_Deserialize_XML/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Xml.Serialization;
+
+
+namespace Learn_Serialize_Deserialize_XML
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("SELAMAT BELAJAR XML SERIALIZE DAN DESERIALIZE\n\n");
+            SerializeObject();
+
+            Console.WriteLine("Processing the xml writer to xml file...");
+            SerializeObjectToXMLFile();
+            Console.WriteLine("Writing to xml file completed...");
+        }
+
+        private static void SerializeObject()
+        {
+            var member = new FT400InitialConfiguration()
+            {
+                Name = "John",
+                Email = "jhon@gmail.com",
+                Age = 22,
+                JoiningDate = DateTime.Now,
+                IsAlive = true
+            };
+
+            var xmlSerializer = new XmlSerializer(typeof(FT400InitialConfiguration));
+            using (var writer = new StringWriter())
+            {
+                xmlSerializer.Serialize(writer, member);
+                var xmlContent = writer.ToString();
+                Console.WriteLine(xmlContent);
+                
+            }
+        }
+
+        private static void SerializeObjectToXMLFile()
+        {
+            var member = new FT400InitialConfiguration()
+            {
+                Name = "Gilang Peradilan Semu",
+                Email = "jhon@gmail.com",
+                Age = 22,
+                JoiningDate = DateTime.Now,
+                IsAlive = true
+            };
+            var xmlSerializer = new XmlSerializer(typeof(FT400InitialConfiguration));
+            using (var writer = new StreamWriter(@"E:\Employee_Gilang\Learn_GitFLow\Serialize_Deserialize_XML\xml_serialize_result\resultMember1.xml"))
+            {
+                xmlSerializer.Serialize(writer, member);
+            }
+        }
+    }
+}
